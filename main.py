eeb0286ef586c672cbc28f9189fe5c93a9dbcba4
Jaralva <zaingrazes@mail.ru>
2023-09-12 23:00:46 +0300

    0.4

--- main.py
+++ main.py
@@ -1,34 +1,63 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
 
 
+main = ReplyKeyboardMarkup(resize_keyboard=True)
+main.add('Каталог').add('Корзина').add('Контакты')
+
+main_admin = ReplyKeyboardMarkup(resize_keyboard=True)
+main_admin.add('Каталог').add('Корзина').add('Контакты').add('Админ-панель')
+
+admin_panel = ReplyKeyboardMarkup(resize_keyboard=True)
+admin_panel.add('Добавить товар').add('Удалить товар').add('Сделать рассылку')
+
+
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    await message.answer_sticker('CAACAgIAAxkBAAMRZPYXr77Jk5to1OxT7IR1tXHedTYAAmkbAALDlFlKR65n-ZmadxIwBA')
-    await message.answer(f'{message.from_user.first_name}, you are welcome')  # Обработка команды /stat
+    await message.answer_sticker('CAACAgIAAxkBAAMpZBAAAfUO9xqQuhom1S8wBMW98ausAAI4CwACTuSZSzKxR9LZT4zQLwQ')
+    await message.answer(f'{message.from_user.first_name}, добро пожаловать в магазин кроссовок!',
+                         reply_markup=main)
+    if message.from_user.id == int(os.getenv('ADMIN_ID')):
+        await message.answer(f'Вы авторизовались как администратор!', reply_markup=main_admin)
 
 
-@dp.message_handler(content_types=['sticker'])
-async def check_sticker(message: types.Message):
-    await message.answer(message.sticker.file_id)
-    await bot.send_message(message.from_user.id, message.chat.id)
-    # бот будет отправлять мне в личные сообщения ID группы
-    # с которой было отправлено это сообщение
+@dp.message_handler(commands=['id'])
+async def cmd_id(message: types.Message):
+    await message.answer(f'{message.from_user.id}')
 
-@dp.message_handler(content_types=['document', 'photo']) # обработчик документов и фото
-async def forward_message(message: types.Message):
-    await bot.forward_message(os.getenv('GROUP_ID'), message.from_user.id, message.message_id)
+
+@dp.message_handler(text='Каталог')
+async def catalog(message: types.Message):
+    await message.answer(f'Каталог пуст!')
+
+
+@dp.message_handler(text='Корзина')
+async def cart(message: types.Message):
+    await message.answer(f'Корзина пуста!')
+
+
+@dp.message_handler(text='Контакты')
+async def contacts(message: types.Message):
+    await message.answer(f'Покупать товар у него: @mesudoteach')
+
+
+@dp.message_handler(text='Админ-панель')
+async def contacts(message: types.Message):
+    if message.from_user.id == int(os.getenv('ADMIN_ID')):
+        await message.answer(f'Вы вошли в админ-панель', reply_markup=admin_panel)
+    else:
+        await message.reply('Я тебя не понимаю.')
+
 
 @dp.message_handler()
 async def answer(message: types.Message):
-    await message.reply('What are you talking about? I dont understand')  # Обработка обычного сообщения
+    await message.reply('Я тебя не понимаю.')
 
 
 if __name__ == '__main__':
